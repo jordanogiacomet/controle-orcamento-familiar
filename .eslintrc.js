c488f6b05df3e67b8996cd8adcd4c743fc0e5a12
jordanogiacomet <110415222+jordanogiacomet@users.noreply.github.com>
2024-03-26 21:18:53 -0300

    WIP - Inicio da criacao de receitaService;

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,14 @@ module.exports = {
     'ecmaVersion': 'latest',
   },
   'rules': {
+    'require-jsdoc': ['error', {
+      'require': {
+        'FunctionDeclaration': true,
+        'MethodDefinition': false,
+        'ClassDeclaration': false,
+        'ArrowFunctionExpression': false,
+        'FunctionExpression': false,
+      },
+    }],
   },
 };
